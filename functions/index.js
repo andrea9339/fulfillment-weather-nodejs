b561c41f855583542153b6406fb8b0a61f5c5712
andrea9339 <156019400+andrea9339@users.noreply.github.com>
2024-06-30 23:34:00 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -16,8 +16,8 @@
 const http = require('http');
 const functions = require('firebase-functions');
 
-const host = 'api.worldweatheronline.com';
-const wwoApiKey = '<ENTER_WWO_API_KEY_HERE>';
+const host = 'https://open-meteo.com/';
+const wwoApiKey = 'https://my-server.tld/v1/forecast?latitude=44.4938&longitude=11.3387&hourly=temperature_2m&forecast_days=1';
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((req, res) => {
   // Get the city and date from the request
